4b8f00532c062d480514ec2fd7624bb2574b3083
dario <darioasaro29@gmail.com>
2020-02-09 18:17:35 -0300

    first

---
+++ first/src/Page.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Page extends Component{
+    render(){
+        return(
+        <div className="header">
+            <img src='./images/cubo.jpg' className="imagenHeader" href="#" />  
+        </div>
+        )
+    }
+}
+export default Page
